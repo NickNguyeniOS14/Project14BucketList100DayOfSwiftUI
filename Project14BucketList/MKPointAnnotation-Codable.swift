8fb6089436397734c135dfe6cca523d6e995032f
Nick <ptnguyen1901@gmail.com>
2020-08-13 10:24:30 -0400

    MKPointAnnotation + extension

---
+++ Project14BucketList/MKPointAnnotation-Codable.swift
@@ -0,0 +1,40 @@
+//
+//  MKPointAnnotation-Codable.swift
+//  Project14BucketList
+//
+//  Created by Nick Nguyen on 8/13/20.
+//  Copyright © 2020 Nick Nguyen. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class CodableMKPointAnnotation: MKPointAnnotation, Codable {
+    enum CodingKeys: CodingKey {
+        case title, subtitle, latitude, longitude
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    required init(from decoder: Decoder) throws {
+        super.init()
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        title = try container.decode(String.self, forKey: .title)
+        subtitle = try container.decode(String.self, forKey: .subtitle)
+        
+        let latitude = try container.decode(CLLocationDegrees.self, forKey: .latitude)
+        let longitude = try container.decode(CLLocationDegrees.self, forKey: .longitude)
+        coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(title, forKey: .title)
+        try container.encode(subtitle, forKey: .subtitle)
+        try container.encode(coordinate.latitude, forKey: .latitude)
+        try container.encode(coordinate.longitude, forKey: .longitude)
+    }
+}
