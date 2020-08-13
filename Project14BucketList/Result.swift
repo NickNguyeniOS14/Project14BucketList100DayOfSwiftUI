70624359da52cbb3ff0ccbdde987c241c5c992e0
Nick <ptnguyen1901@gmail.com>
2020-08-13 10:13:27 -0400

    fetch nearby places

---
+++ Project14BucketList/Result.swift
@@ -0,0 +1,24 @@
+//
+//  Result.swift
+//  Project14BucketList
+//
+//  Created by Nick Nguyen on 8/13/20.
+//  Copyright © 2020 Nick Nguyen. All rights reserved.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
