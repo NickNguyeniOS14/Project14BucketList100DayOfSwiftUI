1ed7f8e1cd7903f063c98866335228c5a22af913
Nick <ptnguyen1901@gmail.com>
2020-08-13 09:50:39 -0400

    add editview

---
+++ Project14BucketList/EditView.swift
@@ -0,0 +1,37 @@
+//
+//  EditView.swift
+//  Project14BucketList
+//
+//  Created by Nick Nguyen on 8/13/20.
+//  Copyright © 2020 Nick Nguyen. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct EditView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var placemark: MKPointAnnotation
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Place name",text: $placemark.wrappedTitle)
+                    TextField("Description",text: $placemark.wrappedSubtitle)
+                }
+            }
+        .navigationBarTitle("Edit place")
+            .navigationBarItems(trailing: Button("Done") {
+                self.presentationMode.wrappedValue.dismiss()
+            })
+        }
+    }
+}
+
+struct EditView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditView(placemark: MKPointAnnotation.example)
+    }
+}
---
+++ Project14BucketList/MKPointAnnotation-ObservableObject.swift
@@ -0,0 +1,30 @@
+//
+//  MKPointAnnotation-ObservableObject.swift
+//  Project14BucketList
+//
+//  Created by Nick Nguyen on 8/13/20.
+//  Copyright © 2020 Nick Nguyen. All rights reserved.
+//
+
+import MapKit
+
+extension MKPointAnnotation: ObservableObject {
+    public var wrappedTitle: String {
+        get {
+            self.title ?? "Unknown value"
+        }
+        set {
+            title = newValue
+        }
+    }
+    
+    
+    public var wrappedSubtitle: String {
+        get {
+            self.subtitle ?? "Unknown value"
+        }
+        set {
+            subtitle = newValue
+        }
+    }
+}
