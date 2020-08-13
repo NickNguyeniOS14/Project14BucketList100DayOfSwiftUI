8084e49e0302a328c1b31012a0afc9c983e50f86
Nick <ptnguyen1901@gmail.com>
2020-08-13 08:31:59 -0400

    Initial Commit

---
+++ Project14BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project14BucketList
+//
+//  Created by Nick Nguyen on 8/13/20.
+//  Copyright © 2020 Nick Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
