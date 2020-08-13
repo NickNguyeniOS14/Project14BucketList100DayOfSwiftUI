0aa6984df6bb133c3a2e2906d6b6320aa451f467
Nick <ptnguyen1901@gmail.com>
2020-08-13 09:42:55 -0400

    drop pin

--- Project14BucketList/MapView.swift
+++ Project14BucketList/MapView.swift
@@ -12,6 +12,9 @@ import SwiftUI
 struct MapView: UIViewRepresentable {
     
     @Binding var centerCoordinate: CLLocationCoordinate2D
+    @Binding var selectedPlace: MKPointAnnotation?
+    @Binding var showingPlaceDetails: Bool
+    
     var annotations: [MKPointAnnotation]
     
     
@@ -42,6 +45,11 @@ struct MapView: UIViewRepresentable {
             return annotationView
         }
         
+        func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+            guard let placemark = view.annotation as? MKPointAnnotation else { return }
+            parent.selectedPlace = placemark
+            parent.showingPlaceDetails = true
+        }
         
     }
     
@@ -88,6 +96,6 @@ extension MKPointAnnotation {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView(centerCoordinate: .constant(MKPointAnnotation.example.coordinate),annotations: [MKPointAnnotation.example])
+        MapView(centerCoordinate: .constant(MKPointAnnotation.example.coordinate), selectedPlace: .constant(MKPointAnnotation.example), showingPlaceDetails: .constant(false), annotations: [MKPointAnnotation.example])
     }
 }
